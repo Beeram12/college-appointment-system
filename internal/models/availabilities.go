ad0087e959aca33523904dd1c798ea30f68f73d7
Beeram_12 <pranithreddy16.beeram@gmail.com>
2025-02-07 20:30:59 +0530

    modified

--- internal/models/availabilities.go
+++ internal/models/availabilities.go
@@ -6,5 +6,5 @@ type Availability struct {
 	Id          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	ProfessorId primitive.ObjectID `json:"professorId" bson:"professor_id"`
 	TimeSlot    string             `json:"timeSlot" bson:"time_slot"`
-	Isbooked    bool               `json:"isBooked" bson:"is_booked"`
+	IsBooked    bool               `json:"isBooked" bson:"is_booked"`
 }
