1aa9f4d584ebdd246704b021b850a2b50de69074
Beeram_12 <pranithreddy16.beeram@gmail.com>
2025-02-08 16:13:57 +0530

    Updated the handler according to time.Time format and added extra debuggin errors

--- internal/handlers/appointment.go
+++ internal/handlers/appointment.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/Beeram12/college-appointment-system/internal/middleware"
 	"github.com/Beeram12/college-appointment-system/internal/models"
@@ -25,16 +27,38 @@ func NewAppointment(repo repository.Appointment) *AppointmentHandler {
 }
 
 func (m *AppointmentHandler) BookAppointment(w http.ResponseWriter, r *http.Request) {
-	_, ok := r.Context().Value(middleware.UserContextKey).(*utils.CustomClaims)
-	if !ok {
+	userClaims, ok := r.Context().Value(middleware.UserContextKey).(*utils.CustomClaims)
+	if !ok || userClaims.Role != "student" {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
-	var appointment models.Appointment
-	err := json.NewDecoder(r.Body).Decode(&appointment)
+	studentID, err := primitive.ObjectIDFromHex(userClaims.UserID)
 	if err != nil {
+		http.Error(w, "Invalid student ID", http.StatusBadRequest)
+		return
+	}
+	var req struct {
+		ProfessorId string `json:"professor_id"`
+		TimeSlot    string `json:"time_sloot"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
+	}
+	professorID, err := primitive.ObjectIDFromHex(req.ProfessorId)
+	if err != nil {
+		http.Error(w, "Invalid professor ID", http.StatusBadRequest)
+		return
+	}
+	parsedTime, err := time.Parse(utils.TimeLayout, req.TimeSlot)
+	if err != nil {
+		http.Error(w, "Invalid time format.Use 'hh:mm AM/PM'", http.StatusBadRequest)
+		return
+	}
+	appointment := models.Appointment{
+		ProfessorId: professorID,
+		StudentId:   studentID,
+		TimeSlot:    parsedTime,
 	}
 	// insert apponitment into db
 	appointmentID, err := m.Repo.BookAppointment(r.Context(), appointment)
@@ -45,24 +69,37 @@ func (m *AppointmentHandler) BookAppointment(w http.ResponseWriter, r *http.Requ
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(map[string]interface{}{
 		"appointment_id": appointmentID.Hex(),
+		"time_slot":      parsedTime.Format(utils.TimeLayout),
 	})
 }
 
 func (m *AppointmentHandler) CancelAppointment(w http.ResponseWriter, r *http.Request) {
 	userClaims, ok := r.Context().Value(middleware.UserContextKey).(*utils.CustomClaims)
-	if !ok {
+	if !ok || userClaims.Role != "professor" {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
 	vars := mux.Vars(r)
-	appointmentID, err := primitive.ObjectIDFromHex(vars["appointment_id"])
+	appointmentIDHex := strings.TrimSpace(vars["appointment_id"])
+	appointmentID, err := primitive.ObjectIDFromHex(appointmentIDHex)
 	if err != nil {
 		http.Error(w, "Invalid appointmentID", http.StatusBadRequest)
 		return
 	}
-	// check roles based access
-	if userClaims.Role != "professor" {
-		http.Error(w, "Permission denied", http.StatusForbidden)
+	// fetch the appointment to verify ownership
+	appointment, err := m.Repo.GetAppointmentByID(r.Context(), appointmentID)
+	if err != nil {
+		http.Error(w, "Appointment not found", http.StatusNotFound)
+		return
+	}
+	professorID, err := primitive.ObjectIDFromHex(userClaims.UserID)
+	if err != nil {
+		http.Error(w, "Invalid Professor ID", http.StatusBadRequest)
+		return
+	}
+	// Ensure the professor is authorized to cancel the appointment
+	if appointment.ProfessorId != professorID {
+		http.Error(w, "You are not authorized to cancel this appointment", http.StatusForbidden)
 		return
 	}
 	// perform cancel operation
@@ -77,18 +114,14 @@ func (m *AppointmentHandler) CancelAppointment(w http.ResponseWriter, r *http.Re
 
 func (m *AppointmentHandler) GetAppointmentsByStudentID(w http.ResponseWriter, r *http.Request) {
 	userClaims, ok := r.Context().Value(middleware.UserContextKey).(*utils.CustomClaims)
-	if !ok {
+	// ensuring the role is student
+	if !ok || userClaims.Role != "student" {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
-
-	// ensuring the role is student
-	if userClaims.Role != "student" {
-		http.Error(w, "Permission denied", http.StatusForbidden)
-		return
-	}
 	// Convert student ID from jwt claims
-	studentID, err := primitive.ObjectIDFromHex(userClaims.Username)
+
+	studentID, err := primitive.ObjectIDFromHex(userClaims.UserID)
 	if err != nil {
 		http.Error(w, "Invalid student ID", http.StatusUnauthorized)
 		return
@@ -99,8 +132,9 @@ func (m *AppointmentHandler) GetAppointmentsByStudentID(w http.ResponseWriter, r
 		http.Error(w, "Failed to fetch appointments", http.StatusInternalServerError)
 		return
 	}
+	for i := range appointments {
+		appointments[i].TimeSlotFormatted = appointments[i].TimeSlot.Format(utils.TimeLayout)
+	}
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(appointments)
 }
-
-
