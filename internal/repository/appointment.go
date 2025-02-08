da493f4d298c55476e5915093471bb181ee00b38
Beeram_12 <pranithreddy16.beeram@gmail.com>
2025-02-08 16:11:53 +0530

    Removed Get appointmentsbyProfessorID function

--- internal/repository/appointment.go
+++ internal/repository/appointment.go
@@ -12,7 +12,6 @@ import (
 type Appointment interface {
 	BookAppointment(ctx context.Context, appointment models.Appointment) (primitive.ObjectID, error)
 	GetAppointmentsOfStudent(ctx context.Context, studentID primitive.ObjectID) ([]models.Appointment, error)
-	GetAppointmentsOfProfessor(ctx context.Context, professorID primitive.ObjectID) ([]models.Appointment, error)
 	CancelAppointment(ctx context.Context, appointmentID primitive.ObjectID) error
+	GetAppointmentByID(ctx context.Context, appointmentID primitive.ObjectID) (*models.Appointment, error)
 }
- 
