08a3b865f673ff0f9a334d23e4b90c1c2d49bd3a
Beeram_12 <pranithreddy16.beeram@gmail.com>
2025-02-08 16:11:06 +0530

    modified schema remove time from string and added it to time.Time

--- internal/models/appointment.go
+++ internal/models/appointment.go
@@ -1,12 +1,15 @@
 package models
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Appointment struct {
-	Id          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	ProfessorId primitive.ObjectID `json:"professorId" bson:"professor_id"`
-	StudentId   primitive.ObjectID `json:"studentId" bson:"student_id"`
-	TimeSlot    string             `json:"timeslot" bson:"time_slot"`
+	Id                primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	ProfessorId       primitive.ObjectID `json:"professorId" bson:"professor_id"`
+	StudentId         primitive.ObjectID `json:"studentId" bson:"student_id"`
+	TimeSlot          time.Time          `json:"timeslot" bson:"time_slot"`
+	TimeSlotFormatted string             `json:"time_slot_formatted" bson:"time_slot_formatted"`
 }
